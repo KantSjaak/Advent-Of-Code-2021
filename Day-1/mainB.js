8068d4b24d480b2cb020a4e456cefcd9f9e98c78
BlazeOnaKayk <kantsjaakcan@gmail.com>
2021-12-01 09:50:22 +0100

    I have less code than EmileDeGamer :D

--- Day-1/mainB.js
+++ Day-1/mainB.js
@@ -4,14 +4,8 @@ let inputArray = fs.readFileSync('input.txt', 'utf8').split('\r\n').map(Number);
 
 let increased = 0;
 
-let numbers = [];
-
-for (let index = 1; index < inputArray.length - 1; index++) {
-    numbers.push(inputArray[index - 1] + inputArray[index] + inputArray[index + 1]);
-}
-
-for (let index = 1; index < numbers.length + 1; index++) {
-    if (numbers[index] > numbers[index - 1]) {
+for (let index = 2; index < inputArray.length + 1; index++) {
+    if (inputArray[index - 1] + inputArray[index] + inputArray[index + 1] > inputArray[index - 2] + inputArray[index - 1] + inputArray[index]) {
         increased++;
     }
 }
